2c66d5f62934798545acfe347f56f4d8b9a97e07
rupigithub <117827907+rupigithub@users.noreply.github.com>
2024-05-15 00:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	   // Convert input strings to Date objects
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    
+    // Calculate the difference in milliseconds
+    const diffInMs = d2 - d1;
+    
+    // Convert milliseconds to days
+    const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    
+    return diffInDays;
 };
 
 // Do not change the code below.
